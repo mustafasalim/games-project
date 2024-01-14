031b4c672c67b5c5f491361be46429a257948154
Mustafa Salim <salimmustafa763@gmail.com>
2024-01-14 16:13:00 +0300

    apı key update

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,72 +1,65 @@
-import React, { useEffect, useState } from 'react'
-import { getPost } from '../../services/post'
-import { Link } from 'react-router-dom'
-import useGenerateRandomColor from '../../components/randomColor'
-import RecommendedGames from '../../components/recomendedGmes'
+import React, { useEffect, useState } from "react"
+import { getPost } from "../../services/post"
+import { Link } from "react-router-dom"
+import useGenerateRandomColor from "../../components/randomColor"
+import RecommendedGames from "../../components/recomendedGmes"
 import { useSelector } from "react-redux"
 
 function Home() {
-    const searchTerm = useSelector((state) => state.items.searchTerm);
+  const searchTerm = useSelector((state) => state.items.searchTerm)
 
+  console.log(process.env.NODE_ENV)
 
+  const { color, generateColor } = useGenerateRandomColor()
 
-    const { color, generateColor } = useGenerateRandomColor();
+  const [gameDetail, setGameDatail] = useState(false)
+  useEffect(() => {
+    getPost().then((res) => setGameDatail(res))
+  }, [])
 
-    const [gameDetail, setGameDatail] = useState(false)
-    useEffect(() => {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <RecommendedGames>
+          <Link to="/">Recommended Games</Link>
+        </RecommendedGames>
+      </div>
 
-        getPost().then(res => setGameDatail(res))
+      <div className=" flex flex-wrap justify-center">
+        {gameDetail &&
+          gameDetail
+            .filter((item) => {
+              return searchTerm.toLowerCase() === ""
+                ? item
+                : item.title.toLowerCase().includes(searchTerm)
+            })
+            .map((users) => (
+              <Link
+                key={users.id}
+                to={users.game_url}
+              >
+                <div className="shadow-2xl m-5 ">
+                  <div
+                    style={{
+                      backgroundColor: "#" + color,
+                    }}
+                    onMouseOver={generateColor}
+                    className="w-[400px]  rounded-md overflow-hidden relative"
+                  >
+                    <img
+                      className="w-full h-full object-cover hover:grayscale transition-all hover:translate-x-2 hover:-translate-y-2"
+                      src={users.thumbnail}
+                      alt=""
+                    />
 
-    }, [])
-
-    return (
-
-        <>
-            <div className='flex items-center justify-center'>
-                <RecommendedGames>
-                    <Link to="/">
-                        Recommended Games
-                    </Link>
-                </RecommendedGames>
-            </div>
-
-            <div className=' flex flex-wrap justify-center'>
-
-                {gameDetail && gameDetail.filter((item) => {
-
-                    return searchTerm.toLowerCase() === "" ? item : item.title.toLowerCase().includes(searchTerm)
-
-                }).map(users => (
-
-                    <Link key={users.id} to={users.game_url}>
-                        <div className='shadow-2xl m-5 '>
-                            <div
-                                style={{
-                                    backgroundColor: "#" + color
-                                }}
-                                onMouseOver={generateColor} className='w-[400px]  rounded-md overflow-hidden relative'>
-                                <img className='w-full h-full object-cover hover:grayscale transition-all hover:translate-x-2 hover:-translate-y-2' src={users.thumbnail} alt="" />
-
-                                <div className=' bottom-0 right-0 w-full h-full '>
-
-                                </div>
-
-                            </div>
-                        </div>
-
-                    </Link>
-                ))}
-
-
-
-
-
-
-            </div >
-        </>
-
-
-    )
+                    <div className=" bottom-0 right-0 w-full h-full "></div>
+                  </div>
+                </div>
+              </Link>
+            ))}
+      </div>
+    </>
+  )
 }
 
 export default Home
--- src/services/request.js
+++ src/services/request.js
@@ -1,34 +1,26 @@
-function request( url , data = false , method = "GET") {
+function request(url, data = false, method = "GET") {
+  return new Promise(async (resolve, reject) => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": import.meta.env.VITE_REACT_API_KEY,
+        "X-RapidAPI-Host": "free-to-play-games-database.p.rapidapi.com",
+      },
+    }
+    const response = await fetch(url, options)
 
+    if (data && method === "POST") {
+      options.body = JSON.stringify(data)
+    }
 
-    return new Promise(async(resolve,reject) =>{
-
-        const options = {
-                    method: 'GET',
-                    headers: {
-                        'X-RapidAPI-Key': 'ea3115c7aamsheb7d526122dd4dfp1419b8jsnb24df3634482',
-                        'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-                    }
-                };
-                const response = await fetch(url,options)
-
-                if(data && method === "POST"){
-
-                    options.body = JSON.stringify(data)
-                }
-
-               
-                const result = await response.json()
-                if(response.ok){
-
-                  
-                    resolve(result)
-                } else {
-
-                    reject(result)
-                }
-    })
+    const result = await response.json()
+    if (response.ok) {
+      resolve(result)
+    } else {
+      reject(result)
+    }
+  })
 }
 
-export const post =(url,data) => request(url,data,"POST")
-export const get = url => request(url)
+export const post = (url, data) => request(url, data, "POST")
+export const get = (url) => request(url)
